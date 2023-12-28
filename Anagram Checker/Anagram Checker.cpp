2d5a9c5811c5d036652e1efe403311358a65b433
Zoltán Gál <oktatonline@gmail.com>
2023-12-28 12:39:52 +0100

    added functionality to be able to check for vectors of words and output the individual anagram groups

--- Anagram Checker/Anagram Checker.cpp
+++ Anagram Checker/Anagram Checker.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <string>
 
 
+using CharIntPairVector = std::vector<std::pair<char, int>>;
 
-std::vector<std::pair<char, int>> letter_frequencies(const std::string& input_word)
+
+CharIntPairVector letter_frequencies(const std::string& input_word)
 {
     std::unordered_map<char, int> word_frequencies{};
-
+    
     // Basic logic for introducing values into the hashmap
     for (char letter : input_word)
     {
@@ -48,17 +51,62 @@ std::vector<std::pair<char, int>> letter_frequencies(const std::string& input_wo
     return result;
 }
 
+std::string vectorToString(const CharIntPairVector& vec) {
+    std::string result;
+    for (const auto& pair : vec) {
+        result += pair.first;
+        result += std::to_string(pair.second);
+    }
+    return result;
+}
+
+std::vector<std::vector<std::string>> anagram_groups(const std::vector<std::string>& input_list) {
+    std::unordered_map<std::string, std::vector<std::string>> hash{};
+    std::vector<std::vector<std::string>> result{};
+
+    for (const std::string& word : input_list) {
+        std::vector<std::pair<char, int>> key = letter_frequencies(word);
+        std::string keyString = vectorToString(key);
+        hash[keyString].push_back(word);
+    }
+
+    for (auto unit : hash) {
+        result.push_back(unit.second);
+    }
+
+    return result;
+}
+
+
 int main()
 {
+
+    std::cout << "CHECKING A SINGLE PAIR OF WORDS FOR ANAGRAMS: \n";
     std::string word1{ "Apple" };
     std::string word2{ "ppleA" };
-
-    using CharIntPairVector = std::vector<std::pair<char, int>>;
-
     CharIntPairVector word1freq{ letter_frequencies(word1) };
     CharIntPairVector word2freq{ letter_frequencies(word2) };
-    std::string result{ word1freq == word2freq ? "The two words are anagrams." : "The two words are NOT anagrams." };
+    std::string result_string{ word1freq == word2freq ? "The two words are anagrams." : "The two words are NOT anagrams." };
+    std::cout << result_string;
 
-    std::cout << result;
+
+    std::cout << "\n\nCHECKING AN ARRAY OF WORDS FOR ANAGRAM GROUPS:\n";
+    std::vector<std::string> inputs{ "act", "cat", "listen", "silent", "enlist", "hello", "world", "dog",
+                                    "god", "heart", "earth", "tear", "break", "baker", "react", "trace", "care",
+                                    "acre", "cinema", "iceman", "debit", "bide", "bore", "robe", "bread", "beard", "angle",
+                                    "glean", "listen", "enlist", "silent", "artist", "traits", "star", "rats", "arts", "post", "stop",
+                                    "tops", "elbow", "below", "bowel", "lamp", "palm", "maple", "ample", "debit", "bide", "cinema", "iceman" };
+
+    std::vector<std::vector<std::string>> result = anagram_groups(inputs);
+
+    for (int i = 0; i < result.size(); ++i)
+    {
+        std::cout << "Group " << i + 1 << ": ";
+        for (const auto& word : result[i])
+        {
+            std::cout << word << " ";
+        }
+        std::cout << std::endl;
+    }
     
 }
